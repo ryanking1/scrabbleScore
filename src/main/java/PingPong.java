d6ea69950d0ae6ddee6a7cd9f51a314d0f905e0e
Andrew Telleria and Ryan King <atelleria08@gmail.com>
2016-02-08 14:17:38 -0800

    Working app and passing specs

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,15 +1,35 @@
-public class PingPong {
-  public static void main(String[] args) {}
+import java.io.Console;
+import java.util.*;
 
-  public static String isPingPong(Integer userNumber) {
-    if (userNumber % 15 == 0) {
-      return "Ping Pong";
-    } else if (userNumber % 3 == 0) {
-      return "ping";
-    } else if (userNumber % 5 == 0) {
-      return "pong";
-    } else {
-      return "false";
+public class PingPong {
+  public static void main(String[] args) {
+    Console console = System.console();
+    int userNumber = 15;
+    //System.out.println("Please Choose a Number:");
+    //String stringUserNumber = console.readLine();
+    //Integer userNumber = Integer.parseInt(stringUserNumber);
+
+    //String[] pingPongList = new String[userNumber];
+}
+    public static ArrayList<String> myArrayMethod(Integer userNumber) {
+    ArrayList<String> pingPongList = new ArrayList<String>();
+    for (int i = 1; i <= userNumber; i++) {
+      if (i % 15 == 0) {
+        pingPongList.add("Ping Pong");
+      } else if (i % 3 == 0) {
+        pingPongList.add("ping");
+      } else if (i % 5 == 0) {
+        pingPongList.add("pong");
+      } else {
+        pingPongList.add(Integer.toString(i));
+      }
+      // System.out.println( pingPongList.get(i) );
     }
+
+    //System.out.println("Ping Pong List:");
+    //for ( Integer i = 0; i < userNumber; i++) {
+    //  System.out.println( pingPongList.get(i) );
+  //  }
+    return pingPongList;
   }
 }
