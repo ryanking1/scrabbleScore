5cef3e14e159e7aa8db5eec2d551c0ba9dc49885
Ryan King and trevor elvey <trevor.elvey@gmail.com>
2016-02-09 11:35:49 -0800

    Fully functional website

--- src/main/java/ScrabbleScore.java
+++ src/main/java/ScrabbleScore.java
@@ -1,40 +1,37 @@
 import java.io.Console;
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 
 public class ScrabbleScore {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String word = request.queryParams("word");
+      int score = inputWord(word);
+      model.put("word", score);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 
     public static int inputWord(String word) {
       int score = 0;
       HashMap<String, Integer> hmap = new HashMap<String, Integer>();
-      // hmap.put(0, "a");
-      // hmap.put(1, "e");
-      // hmap.put(2, "i");
-      // hmap.put(3, "o");
-      // hmap.put(4, "u");
-      // hmap.put(5, "l");
-      // hmap.put(6, "n");
-      // hmap.put(7, "s");
-      // hmap.put(8, "r");
-      // hmap.put(9, "t");
-      // hmap.put(10, "d");//2
-      // hmap.put(11, "g");
-      // hmap.put(12, "b");//3
-      // hmap.put(13, "c");
-      // hmap.put(14, "m");
-      // hmap.put(15, "p");
-      // hmap.put(16, "f");//4
-      // hmap.put(17, "h");
-      // hmap.put(18, "v");
-      // hmap.put(19, "w");
-      // hmap.put(20, "y");
-      // hmap.put(21, "k");//5
-      // hmap.put(22, "j");//8
-      // hmap.put(23, "x");
-      // hmap.put(24, "q");//10
-      // hmap.put(25, "z");
+
 
       hmap.put("a", 1);
       hmap.put("e", 1);
